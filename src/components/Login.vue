825e5141b97d9e78c760309a434578481cd3f1bd
奉昱晟 <fych@hotmail.com>
2021-02-01 17:08:22 +0800

    add loginbox

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,8 +3,86 @@
     <div class="login-ct">
       <div class="title">Login Demo</div>
       <div class="login">
-        <div class="login-icon"></div>
-        <img alt="Vue logo" src=".././assets/logo.png" />
+        <div class="login-icon">
+          <img alt="Vue logo" src=".././assets/logo.png" />
+        </div>
+        <div class="login-item">
+          <a-form
+            id="components-form-demo-normal-login"
+            :form="form"
+            class="login-form"
+            @submit="handleSubmit"
+          >
+            <a-form-item>
+              <a-input
+                v-decorator="[
+                  'userName',
+                  {
+                    rules: [
+                      {
+                        required: true,
+                        message: 'Please input your username!',
+                      },
+                    ],
+                  },
+                ]"
+                placeholder="Username"
+              >
+                <a-icon
+                  slot="prefix"
+                  type="user"
+                  style="color: rgba(0, 0, 0, 0.25)"
+                />
+              </a-input>
+            </a-form-item>
+            <a-form-item>
+              <a-input
+                v-decorator="[
+                  'password',
+                  {
+                    rules: [
+                      {
+                        required: true,
+                        message: 'Please input your Password!',
+                      },
+                    ],
+                  },
+                ]"
+                type="password"
+                placeholder="Password"
+              >
+                <a-icon
+                  slot="prefix"
+                  type="lock"
+                  style="color: rgba(0, 0, 0, 0.25)"
+                />
+              </a-input>
+            </a-form-item>
+            <a-form-item>
+              <a-checkbox
+                v-decorator="[
+                  'remember',
+                  {
+                    valuePropName: 'checked',
+                    initialValue: true,
+                  },
+                ]"
+              >
+                Remember me
+              </a-checkbox>
+              <a class="login-form-forgot" href=""> Forgot password </a>
+              <a-button
+                type="primary"
+                html-type="submit"
+                class="login-form-button"
+              >
+                Log in
+              </a-button>
+              Or
+              <a href=""> register now! </a>
+            </a-form-item>
+          </a-form>
+        </div>
       </div>
     </div>
   </div>
@@ -27,7 +105,8 @@ export default {};
     text-align: center;
     background-color: #ffffff;
     height: 350px;
-    width: 600px;
+    width: 800px;
+    border-radius: 30px;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -35,11 +114,11 @@ export default {};
 
     .title {
       font-size: 30px;
+      height: 66px;
     }
 
-    .login{
+    .login {
       display: flex;
-      
     }
   }
 }
